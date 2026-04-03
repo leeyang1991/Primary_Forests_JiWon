ec8bc3cbb0b15425c5086d9f990b0a8d4cecf9e2
Yang-M4Pro <leeyang1991@gmail.com>
2026-04-02 23:11:48 -0400

    Bug fixed Download Google Foundation Embeddings

--- Google_Alpha_Earth/download.py
+++ Google_Alpha_Earth/download.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("..")
 from utils import *
 from pprint import pprint
 import boto3
@@ -35,37 +37,42 @@ class Download:
         T.print_head_n(df)
         # T.df_to_excel(df,fpath)
         # exit()
-        utm_zone = T.get_df_unique_val_list(df,'utm_zone')
+        # utm_zone = T.get_df_unique_val_list(df,'utm_zone')
 
         path_list = df['path'].tolist()
         year_list = df['year'].tolist()
 
-        utm_east_list = df['utm_east'].tolist()
-        utm_north_list = df['utm_north'].tolist()
-        utm_south_list = df['utm_south'].tolist()
-        utm_west_list = df['utm_west'].tolist()
+        # utm_east_list = df['utm_east'].tolist()
+        # utm_north_list = df['utm_north'].tolist()
+        # utm_south_list = df['utm_south'].tolist()
+        # utm_west_list = df['utm_west'].tolist()
+
+        wgs_east_list = df['wgs84_east'].tolist()
+        wgs_north_list = df['wgs84_nort'].tolist()
+        wgs_south_list = df['wgs84_sout'].tolist()
+        wgs_west_list = df['wgs84_west'].tolist()
 
         params_list = []
         for i in range(len(path_list)):
             path = path_list[i]
             year = year_list[i]
-            east = utm_east_list[i]
-            north = utm_north_list[i]
-            south = utm_south_list[i]
-            west = utm_west_list[i]
+            east = wgs_east_list[i]
+            north = wgs_north_list[i]
+            south = wgs_south_list[i]
+            west = wgs_west_list[i]
             params_list.append((path,year,east,north,south,west,outdir))
 
         job_name = 'GAE_down'
         log_folder = join(self.data_dir, 'log/download_tiles')
         init_job(job_name, params_list)
         sumbit_jobs_array(self.kernel_download_tiles, params_list, log_folder, job_name=job_name,
-                          job_number_limit=1,
-                          parallel_process_per_task=10,
-                          slurm_array_parallelism=20,
+                          job_number_limit=10,
+                          parallel_process_per_task=5,
+                          slurm_array_parallelism=10,
                           parallel_process_p_or_t='t',
                           cpus_per_task=2,
-                          mem_gb=2,
-                          timeout_min=20,
+                          mem_gb=4,
+                          timeout_min=100,
                           slurm_partition="general",
                           # slurm_partition="debug",
                           exclude_nodes="cn[472,636]",
